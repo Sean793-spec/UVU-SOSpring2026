cd7791864e371411b55b88c1d673eb09001e0146
Sean O'Hara <10890388@uvu.edu>
2026-02-05 09:31:56 -0700

    Unity adding timer and skybox asset

---
+++ Unity/SpringGameProject/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+   [SerializeField] float remainingTime;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime <= 0)
+        {
+            remainingTime = 0;
+            //game over
+            timerText.text = "00:00";
+            timerText.color = Color.red;
+        }
+
+        remainingTime -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(remainingTime / 60f);
+        int seconds = Mathf.FloorToInt(remainingTime % 60f);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
